85d67f77c5e05c40bf37ffcde4e4169fe30d7a3a
Wallace Gomes <gomes.wallace@hotmail.com>
2019-02-17 14:58:03 -0300

    Criação do projeto

---
+++ src/main/java/com/wallacegomes/automacaoedificacoes/AutomacaoEdificacoesApplication.java
@@ -0,0 +1,13 @@
+package com.wallacegomes.automacaoedificacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomacaoEdificacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomacaoEdificacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wallacegomes/automacaoedificacoes/AutomacaoEdificacoesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wallacegomes.automacaoedificacoes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AutomacaoEdificacoesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
