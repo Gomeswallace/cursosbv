03c26e28a2668bbae760ca5cac2e6ac2657a3b07
Wallace Gomes <gomes.wallace@hotmail.com>
2019-02-24 11:49:01 -0300

    Configuracao do banco H2 e criacao automatica da base de dados

--- src/main/java/com/wallacegomes/automacaoedificacoes/resources/EquipamentoResource.java
+++ src/main/java/com/wallacegomes/automacaoedificacoes/resources/EquipamentoResource.java
@@ -1,15 +1,30 @@
 package com.wallacegomes.automacaoedificacoes.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.wallacegomes.automacaoedificacoes.domain.Equipamento;
 
 @RestController
 @RequestMapping(value="/equipamentos")
 public class EquipamentoResource {
 	
+	private Integer id;
+	private String nome;
+	
 	@RequestMapping(method=RequestMethod.GET)  
-	public String listar() {
-		return "REST está funcionando";
+	public List<Equipamento> listar() {
+		Equipamento cat1 = new Equipamento(1, "Informatica");
+		Equipamento cat2 = new Equipamento(1, "Escritorio");
+
+		List<Equipamento> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);		
+
+		return lista;
 	}
 }
